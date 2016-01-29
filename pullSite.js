8f569e389b2078fd1bc0864f8b6d9452f14fe834
y3sh <joshhibschman@gmail.com>
2016-01-29 08:30:23 -0600

    updates

--- pullSite.js
+++ pullSite.js
@@ -8,8 +8,10 @@ var mkdirp = require('mkdirp');
 var moment = require('moment');
 var htmlMinify = require('html-minifier').minify;
 
-var host = "tumblr.com";
-var path = "/";
+var host = "localhost:3000"
+var path = "/ipad/ipad.html";
+//var host = "tumblr.com";
+//var path = "/";
 //var host = "kickstarter.com";
 //var path = "/projects/khaoskreator/shades-of-a-omnibus-limited-edition-hardback.html?ref=popular";
 
@@ -43,30 +45,30 @@ request(opts, function (err, res, body) {
   body = htmlMinify(body, {
     removeComments: true,
     minifyJS: {
-			fromString: true,
-			warnings: true,
-			mangle: false,
-			compress: {
-				sequences: false,  // join consecutive statemets with the “comma operator”
-				properties: false,  // optimize property access: a["foo"] → a.foo
-				dead_code: false,  // discard unreachable code
-				drop_debugger: false,  // discard “debugger” statements
-				unsafe: false, // some unsafe optimizations (see below)
-				conditionals: false,  // optimize if-s and conditional expressions
-				comparisons: false,  // optimize comparisons
-				evaluate: false,  // evaluate constant expressions
-				booleans: false,  // optimize boolean expressions
-				loops: false,  // optimize loops
-				unused: false,  // drop unused variables/functions
-				hoist_funs: false,  // hoist function declarations
-				hoist_vars: false, // hoist variable declarations
-				if_return: false,  // optimize if-s followed by return/continue
-				join_vars: false,  // join var declarations
-				cascade: false,  // try to cascade `right` into `left` in sequences
-				side_effects: false,  // drop side-effect-free statements
-				warnings: true,  // warn about potentially dangerous optimizations/code
-				global_defs: {}     // global definitions
-			}
+      fromString: true,
+      warnings: true,
+      mangle: false,
+      compress: {
+        sequences: false,  // join consecutive statemets with the “comma operator”
+        properties: false,  // optimize property access: a["foo"] → a.foo
+        dead_code: false,  // discard unreachable code
+        drop_debugger: false,  // discard “debugger” statements
+        unsafe: false, // some unsafe optimizations (see below)
+        conditionals: false,  // optimize if-s and conditional expressions
+        comparisons: false,  // optimize comparisons
+        evaluate: false,  // evaluate constant expressions
+        booleans: false,  // optimize boolean expressions
+        loops: false,  // optimize loops
+        unused: false,  // drop unused variables/functions
+        hoist_funs: false,  // hoist function declarations
+        hoist_vars: false, // hoist variable declarations
+        if_return: false,  // optimize if-s followed by return/continue
+        join_vars: false,  // join var declarations
+        cascade: false,  // try to cascade `right` into `left` in sequences
+        side_effects: false,  // drop side-effect-free statements
+        warnings: true,  // warn about potentially dangerous optimizations/code
+        global_defs: {}     // global definitions
+      }
     }
   });
 
@@ -85,6 +87,10 @@ request(opts, function (err, res, body) {
       resourceLink = $domItem.attr("src");
     } else if ($domItem.attr("src")) {
       resourceLink = $domItem.attr("src");
+    } else if ($domItem.attr("data-mp4")) {
+      resourceLink = $domItem.attr("data-mp4");
+    } else if ($domItem.attr("data-touch-media")) {
+      resourceLink = $domItem.attr("data-touch-media");
     }
 
     if (resourceLink && resourceLink.indexOf("http") < 0) {
@@ -114,6 +120,10 @@ request(opts, function (err, res, body) {
         $domItem.attr("src", fileName);
       } else if ($domItem.attr("src")) {
         $domItem.attr("src", fileName);
+      } else if ($domItem.attr("data-mp4")) {
+        $domItem.attr("data-mp4", fileName);
+      } else if ($domItem.attr("data-touch-media")) {
+        $domItem.attr("data-touch-media", fileName);
       }
 
       console.log(resourceLink, fileName);
